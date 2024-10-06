4935d9798cba699e07e40534af425c03f70f7f19
Takumasa Matsuura <tamatsuu@student.42tokyo.jp>
2024-10-06 23:39:36 +0900

    fixed execvp to execve for mandatory

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tamatsuu <tamatsuu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: tamatsuu <tamatsuu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 03:04:18 by tamatsuu          #+#    #+#             */
-/*   Updated: 2024/10/06 04:26:18 by tamatsuu         ###   ########.fr       */
+/*   Updated: 2024/10/06 23:29:47 by tamatsuu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ int	main(int argc, char *argv[], char *envp[])
 	init_path(envp, &path);
 	init_pipex(argc, argv, &pipe_i, path);
 	validate_args(pipe_i);
-	ret = pipex(pipe_i, path);
+	ret = pipex(pipe_i, path, envp);
 	close_pipex(pipe_i, ret);
 }
--- src/validation_2.c
+++ src/validation_2.c
@@ -6,7 +6,7 @@
 /*   By: tamatsuu <tamatsuu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 14:42:47 by tamatsuu          #+#    #+#             */
-/*   Updated: 2024/10/06 18:30:05 by tamatsuu         ###   ########.fr       */
+/*   Updated: 2024/10/06 23:34:53 by tamatsuu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,21 @@
 #include <stdio.h>
 #include "../libft/libft.h"
 
-void	cmd_executable_check(char **cmd, char **cmd_path)
+void	cmd_executable_check(char **cmd, char **cmd_path, char **exec_cmd)
 {
 	int		exec_status;
 
 	exec_status = 0;
 	if (!cmd)
 		throw_err(NULL, BASH_GENERAL_ERR);
-	if (!check_cmd_exec(cmd[0]) || !cmd_path)
-		exec_status = check_cmd_exec(cmd[0]);
+	if (!check_cmd_exec(cmd[0], exec_cmd) || !cmd_path)
+		exec_status = check_cmd_exec(cmd[0], exec_cmd);
 	else if (ft_strchr(cmd[0], '/'))
-		exec_status = check_cmd_exec(cmd[0]);
+		exec_status = check_cmd_exec(cmd[0], exec_cmd);
 	else if (!cmd[0])
 		exec_status = CMD_NOT_FOUND;
 	else
-		exec_status = check_cmd_exec_w_path(cmd_path, cmd[0]);
+		exec_status = check_cmd_exec_w_path(cmd_path, cmd[0], exec_cmd);
 	if (exec_status == PERMISSION_DENIED)
 	{
 		ft_printerr(PERMISSION_DENIED_MSG, cmd[0]);
@@ -43,41 +43,45 @@ void	cmd_executable_check(char **cmd, char **cmd_path)
 	}
 }
 
-int	check_cmd_exec(char *cmd)
+int	check_cmd_exec(char *cmd, char **exec_cmd)
 {
 	if (access(cmd, F_OK) == 0)
 	{
 		if (access(cmd, X_OK) == 0)
+		{
+			*exec_cmd = ft_strdup(cmd);
 			return (0);
+		}
 		return (PERMISSION_DENIED);
 	}
+	*exec_cmd = NULL;
 	return (CMD_NOT_FOUND);
 }
 
-int	check_cmd_exec_w_path(char **cmd_path, char *cmd)
+int	check_cmd_exec_w_path(char **cmd_path, char *cmd, char **ex_cmd)
 {
 	int		i;
 	int		exec_status;
-	char	*temp;
-	char	*temp2;
+	char	*tmp;
+	char	*tmp2;
 
 	i = 0;
 	exec_status = CMD_NOT_FOUND;
 	while (cmd_path[i])
 	{
-		temp = ft_strjoin(cmd_path[i], "/");
-		temp2 = ft_strjoin(temp, cmd);
-		if (access(temp2, F_OK) == 0)
+		tmp = ft_strjoin(cmd_path[i], "/");
+		tmp2 = ft_strjoin(tmp, cmd);
+		if (access(tmp2, F_OK) == 0)
 		{
-			if (access(temp2, X_OK) == 0)
-				return (free(temp), free(temp2), 0);
+			if (access(tmp2, X_OK) == 0)
+				return (*ex_cmd = ft_strdup(tmp2), free(tmp), free(tmp2), 0);
 			exec_status = PERMISSION_DENIED;
 		}
 		i++;
-		free(temp);
-		free(temp2);
-		temp = NULL;
-		temp2 = NULL;
+		free(tmp);
+		free(tmp2);
+		tmp = NULL;
+		tmp2 = NULL;
 	}
 	return (exec_status);
 }
